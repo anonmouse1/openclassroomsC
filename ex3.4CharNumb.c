2cb04ff39c000b12ac9531a570acda03ee0c8251
anonmouse1 <90865869+anonmouse1@users.noreply.github.com>
2023-06-10 20:12:52 +1000

    char numb print example

---
+++ ex3.4CharNumb.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	char lettreMaj = 'B';
+	char lettreMin = 'b';
+	
+	printf("Value of B: %d\n", lettreMaj);
+	printf("Value of b: %d\n",lettreMin);
+	
+	return 0;
+}
+
+
