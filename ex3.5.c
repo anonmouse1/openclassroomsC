b15b68fb1d491db58948d4f295c635bdb867fb81
anonmouse1 <90865869+anonmouse1@users.noreply.github.com>
2023-06-11 20:53:51 +1000

    x

--- ex3.5.c
+++ ex3.5.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 # define MAJEUR(age) if (age >= 18) \ 
-			printf( "Vous etes majeur\n");
+			printf( "Vous etes majeur\n");\
+		else \
+			printf("Vous etes mineur\n");
 
 int main( int argc, char *argv[])
 {
-	MAJEUR(22);
+	MAJEUR(2);
 
 	return 0;
 }
